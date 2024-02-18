134f00c305a6f31d2ddf124336477ca146c25126
Frank Burnham <frank@n8git.dev>
2024-02-18 08:07:07 -0500

    Compile component aot

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 set -euo pipefail
 
+clj -M -e "(compile 'com.stuartsierra.component)"
+clj -M -e "(compile 'com.stuartsierra.dependency)"
 clj -M -e "(compile 'speakeasy.core)"
 clj -M:uberjar --main-class speakeasy.core
